d7dbf1a4bcc82bb237164e2f168d52f06c5df642
Hua Cai <huacai@10-200-199-59-eduroam.staff.utas.edu.au>
2019-08-23 12:17:08 +1000

    basic structure

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {ingredient} from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients : ingredient[]=[
+    new ingredient('apples',5),
+    new ingredient('oranges',8)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
